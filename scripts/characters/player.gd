155a589d17356db19399ec6ecc02e2d52b2ce664
Marcel W <112943380+o11899nine@users.noreply.github.com>
2024-05-05 21:38:39 +0200

    fix errors

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -11,19 +11,25 @@ const MOVEMENT = {
 	"move_right": Vector2.RIGHT,
 }
 
-func can_move(direction):
-	var vector_pos = MOVEMENT[direction] * GRID_SIZE
-	ray.set_target_position(vector_pos)
+
+func _unhandled_input(event):
+	for direction in MOVEMENT:
+		print(direction)
+		if event.is_action_pressed(direction):
+			move(direction)
+			
+
+func move(direction):
+	
+	var target_position = MOVEMENT[direction] * GRID_SIZE
+	ray.set_target_position(target_position)
 	ray.force_raycast_update()
+	
 	if not ray.is_colliding():
-		return true
+		position += target_position
 	else:
 		var collider = ray.get_collider()
 		if collider.has_method("can_move"):
 			if collider.can_move(direction):
 				collider.move(direction)
-				return true
-
-func move(direction):
-	position += MOVEMENT[direction] * GRID_SIZE
-		
+				position += target_position
