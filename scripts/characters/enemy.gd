58e756cf6bc47b8f1336f43429c1826645082617
Marcel W <112943380+o11899nine@users.noreply.github.com>
2024-05-06 08:55:59 +0200

    refactor

--- scripts/characters/enemy.gd
+++ scripts/characters/enemy.gd
@@ -4,6 +4,8 @@ var grid_size = Globals.GRID_SIZE
 
 @onready var ray = $RayCast2D
 
+signal die
+
 const MOVEMENT = {
 	"move_north": Vector2(0,-1),
 	"move_south": Vector2(0,1),
@@ -18,22 +20,13 @@ const MOVEMENT = {
 
 func _process(delta):
 	
-	var can_move = false
-	var possible_moves = MOVEMENT.keys()
-	var direction = null
+	var valid_move = find_valid_move()
 	
-	for i in possible_moves.size():
-		possible_moves.shuffle()
-		direction = possible_moves[0]
-		possible_moves.pop_front()
-		if is_valid_move(direction):
-			can_move = true
-			break
-	
-	if not can_move:
+	if not valid_move:
 		queue_free()
 		
-func _on_move_cooldown_timeout():
+
+func find_valid_move():
 	
 	var can_move = false
 	var possible_moves = MOVEMENT.keys()
@@ -44,12 +37,14 @@ func _on_move_cooldown_timeout():
 		direction = possible_moves[0]
 		possible_moves.pop_front()
 		if is_valid_move(direction):
-			#var vector_pos = MOVEMENT[direction] * grid_size
-			#var future_position = position + vector_pos
-			can_move = true
-			break
+			return direction
+	return false
 	
-	if can_move:
+func _on_move_cooldown_timeout():
+	
+	var direction = find_valid_move()
+	
+	if direction:
 		move(direction)
 
 func is_valid_move(direction):
@@ -63,17 +58,9 @@ func is_valid_move(direction):
 			
 
 func move(direction):
-	
-	var vector_pos = MOVEMENT[direction] * grid_size
-	ray.set_target_position(vector_pos)
-	ray.force_raycast_update()
-	
-	if not ray.is_colliding():
-		position += vector_pos
-	else:
-		var collider = ray.get_collider()
-		if collider.is_in_group("moveable_objects"):
-			if collider.is_valid_move(direction):
-				collider.move(direction)
-				position += vector_pos
+	position += MOVEMENT[direction] * grid_size
 
+
+
+func _on_die():
+	queue_free()
